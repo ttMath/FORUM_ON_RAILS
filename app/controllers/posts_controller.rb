0f9ee34d53b1d652392e4b6ddd37579db2f54cca
Matheus <tt.math@disroot.org>
2023-03-18 22:05:10 -0300

    created view of create post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,55 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
 
   def show
+    @post = Post.find(id)
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(Post_params)
+
+    if @post.save
+      redirect_to @customer, notice: "aew, post criado."
+    else
+      render :new
+    end      
   end
 
+  def edit
+    @post = Post.find(id)
+  end
+  
+
   def update
+    @post = Post.find(id)
+
+    if @post.update(Post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   def delete
+    @post = Post.find(id)
+    @post.destroy
+    redirect_to posts_path, notice: 'post deletado com sucesso.'
   end
+
+  private
+
+  def Post_params
+    params.require(:post).permit(
+      :title,
+      :body
+    )
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/delete'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts
 end
