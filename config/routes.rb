a0162e8092ed7033b817f4844c4fdcfeb4146634
Matheus <tt.math@disroot.org>
2023-04-07 17:31:22 -0300

    add name to users and configurated controllers devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users
+  devise_for :users, controllers:{
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :posts
+
 
 end
